720c7e39a94e604a3ebc1f8dbaee9dc594487c17
Jonathan Tan <jonathan.tan@nethermind.io>
2024-07-23 15:40:30 +0800

    remove unnecessary columns

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,7 +1,6 @@
 use crate::types::BlockDetails;
 use crate::types::BlockHeaderWithFullTransaction;
 use anyhow::{Context, Result};
-use chrono::{TimeZone, Utc};
 use log::{info, warn};
 use sqlx::postgres::PgConnectOptions;
 use sqlx::ConnectOptions;
@@ -91,54 +90,21 @@ pub async fn write_blockheader(block_header: BlockHeaderWithFullTransaction) -> 
     let result = sqlx::query(
         r#"
         INSERT INTO blockheaders (
-            author, block_hash, number, parent_hash, beneficiary, gas_limit, gas_used, 
-            timestamp, extra_data, difficulty, mix_hash, nonce, uncles_hash, 
-            transaction_root, receipts_root, state_root, base_fee_per_gas, 
-            withdrawals_root, parent_beacon_block_root, blob_gas_used, 
-            excess_blob_gas, total_difficulty, step, signature
+            block_hash, number, gas_limit, gas_used, nonce,
+            transaction_root, receipts_root, state_root
         )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18,
-                $19, $20, $21, $22, $23, $24)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
         ON CONFLICT (block_hash) DO NOTHING
         "#,
     )
-    .bind(&block_header.author)
     .bind(&block_header.hash)
     .bind(convert_hex_string_to_i64(&block_header.number))
-    .bind(&block_header.parent_hash)
-    .bind(&block_header.author)
     .bind(convert_hex_string_to_i64(&block_header.gas_limit))
     .bind(convert_hex_string_to_i64(&block_header.gas_used))
-    .bind(
-        Utc.timestamp_opt(convert_hex_string_to_i64(&block_header.timestamp), 0)
-            .single()
-            .context("Invalid timestamp")?,
-    )
-    .bind(convert_hex_string_to_bytes(&block_header.extra_data))
-    .bind(convert_hex_string_to_i64(&block_header.difficulty))
-    .bind(&block_header.mix_hash)
     .bind(&block_header.nonce)
-    .bind(&block_header.sha3_uncles)
     .bind(&block_header.transactions_root)
     .bind(&block_header.receipts_root)
     .bind(&block_header.state_root)
-    .bind(option_fn_handler(
-        convert_hex_string_to_i64,
-        block_header.base_fee_per_gas.as_deref(),
-    ))
-    .bind(&block_header.withdrawals_root)
-    .bind(&block_header.parent_beacon_block_root)
-    .bind(&block_header.blob_gas_used)
-    .bind(&block_header.excess_blob_gas)
-    .bind(&block_header.total_difficulty)
-    .bind(option_fn_handler(
-        convert_hex_string_to_i64,
-        block_header.step.as_deref(),
-    ))
-    .bind(option_fn_handler(
-        convert_hex_string_to_bytes,
-        block_header.signature.as_deref(),
-    ))
     .execute(&mut *tx) // Changed this line
     .await
     .context("Failed to insert block header")?;
@@ -160,19 +126,15 @@ pub async fn write_blockheader(block_header: BlockHeaderWithFullTransaction) -> 
     if !block_header.transactions.is_empty() {
         let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new(
             "INSERT INTO transactions (
-                block_number, block_hash, transaction_hash, mint, source_hash, nonce, 
+                block_number, transaction_hash,
                 transaction_index, from_addr, to_addr, value, gas_price, max_priority_fee_per_gas, 
-                max_fee_per_gas, gas, input, chain_id, type, v
+                max_fee_per_gas, gas, chain_id
             ) ",
         );
 
         query_builder.push_values(block_header.transactions.iter(), |mut b, tx| {
             b.push_bind(convert_hex_string_to_i64(&tx.block_number))
-                .push_bind(&tx.block_hash)
                 .push_bind(&tx.hash)
-                .push_bind(&tx.mint)
-                .push_bind(&tx.source_hash)
-                .push_bind(&tx.nonce)
                 .push_bind(convert_hex_string_to_i64(&tx.transaction_index))
                 .push_bind(&tx.from)
                 .push_bind(&tx.to)
@@ -181,10 +143,7 @@ pub async fn write_blockheader(block_header: BlockHeaderWithFullTransaction) -> 
                 .push_bind(&tx.max_priority_fee_per_gas)
                 .push_bind(&tx.max_fee_per_gas)
                 .push_bind(&tx.gas)
-                .push_bind(convert_hex_string_to_bytes(&tx.input))
-                .push_bind(&tx.chain_id)
-                .push_bind(convert_hex_string_to_i64(&tx.r#type))
-                .push_bind(&tx.v);
+                .push_bind(&tx.chain_id);
         });
 
         query_builder.push(" ON CONFLICT (transaction_hash) DO NOTHING");
@@ -227,15 +186,4 @@ pub async fn get_blockheaders(start_blocknumber: i64) -> Result<Vec<BlockDetails
 // Helper functions
 fn convert_hex_string_to_i64(hex_str: &str) -> i64 {
     i64::from_str_radix(hex_str.trim_start_matches("0x"), 16).expect("Invalid hex string")
-}
-
-fn convert_hex_string_to_bytes(hex_str: &str) -> Vec<u8> {
-    hex::decode(hex_str.trim_start_matches("0x")).expect("Invalid hex string")
-}
-
-fn option_fn_handler<F, T>(f: F, option: Option<&str>) -> Option<T>
-where
-    F: Fn(&str) -> T,
-{
-    option.map(f)
 }
